4ad4ff673e41e8eca81b83f1c9d39c1389157860
EdmonAvetisyan <edmonavetisyan9@gmail.com>
2021-04-17 16:45:24 +0400

    start grid, wookmark plugin

---
+++ js/index.js
@@ -0,0 +1,22 @@
+window.onload = function () {
+  var wookmark1 = new Wookmark('#wookmark', {
+    // itemWidth: 210,
+
+    offset: 30,
+    // flexibleWidth: 0,
+    // align: 'center',
+    // autoResize: false,
+    // comparator: null,
+    // direction: undefined,
+    // ignoreInactiveItems: true,
+    itemWidth: 210,
+    // fillEmptySpace: false,
+    // onLayoutChanged: undefined,
+    // outerOffset: 200,
+    // possibleFilters: [],
+    // resizeDelay: 10,
+    // verticalOffset: undefined
+
+
+  });
+};
